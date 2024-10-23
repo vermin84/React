697845b7c188c920c6b39bfe45a147fc9359a7cd
vermin84 <verminator84@gmail.com>
2024-10-23 13:11:07 +0300

    Delete online-cinema directory

--- online-cinema/assets/SearchResults-1174f72e.js
+++
@@ -1,1 +0,0 @@
-import{h as o,u as i,j as s,L as n,M as u}from"./index-f9467390.js";import{G as m}from"./GridContainer-c82f3b7f.js";import{M as d}from"./MovieCard-c777be8b.js";function p(){const{results:r}=o(),{data:e,isLoading:t}=i(["search all movie list",r],()=>u(r));return t?s.jsx(n,{}):s.jsx(m,{children:e.map(a=>s.jsx(d,{movie:a},a.id))})}export{p as default};
